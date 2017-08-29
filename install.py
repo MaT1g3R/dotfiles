02d37cc9e0aa8da9de4203ac8346728775926be0
Unknown <982341777@qq.com>
2017-08-29 06:08:40 -0400

    Update install script

--- install.py
+++ install.py
@@ -1,5 +1,31 @@
 #!/bin/env python3
+
+"""
+MIT License
+
+Copyright (c) 2017 MaT1g3R
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from argparse import ArgumentParser
+from collections import OrderedDict
 from json import load
 from os import devnull
 from pathlib import Path
@@ -10,55 +36,46 @@ home = str(Path.home())
 
 def main():
     with here.joinpath('mapping.json').open() as f:
-        mapping = load(f)
-
+        mapping = load(f, object_pairs_hook=OrderedDict)
     parse = ArgumentParser(description='Dotfiles installer')
-
     parse.add_argument(
         '-v', '--verbose', help='Verbose mode', action='store_true'
     )
-
     parse.add_argument(
         '-f', '--force', help='Force mode, remove existing configs',
         action='store_true'
     )
-
-    parse.add_argument(
-        '-common', help='Install common dotfiles', action='store_true'
-    )
-
-    parse.add_argument(
-        '-kde', help='Install KDE specific dotfiles', action='store_true'
-    )
-
-    parse.add_argument(
-        '-linux', help='Install Linux specific dotfiles', action='store_true'
-    )
-
-    parse.add_argument(
-        '-macos', help='Install macOS specific dotfiles',
-        action='store_true'
-    )
+    for key in mapping:
+        parse.add_argument(
+            '-{}'.format(key),
+            help='Install {} dotfiles'.format(prettify(key)),
+            action='store_true'
+        )
 
     args = parse.parse_args()
-
-    if not args.common and not args.kde and not args.macos and not args.linux:
+    types = vars(args)
+    verbose = types.pop('verbose')
+    force = types.pop('force')
+    if not any(types.values()):
+        print('You must choose at lesat one type of dotfiles to install.')
         parse.print_help()
         return
 
     print('Installing...')
-    if args.common:
-        link(args.verbose, args.force, mapping['common'])
-
-    if args.kde:
-        link(args.verbose, args.force, mapping['kde'])
-
-    if args.linux:
-        link(args.verbose, args.force, mapping['linux'])
-
-    if args.macos:
-        link(args.verbose, args.force, mapping['macos'])
+    for key, val in types.items():
+        if val:
+            link(verbose, force, mapping[key])
     print('Done!')
+
+
+def prettify(s):
+    if s == 'linux':
+        return 'GNU/Linux'
+    if s == 'macos':
+        return 'macOS'
+    if s == 'kde':
+        return 'KDE'
+    return s
 
 
 def link(verbose, force, files):
@@ -72,6 +89,7 @@ def link(verbose, force, files):
 
 def link_one(verbose, force, src, dest):
     out = None if verbose else open(devnull, 'w')
+
     if not isinstance(src, Path):
         src = here.joinpath(src).resolve()
     if not isinstance(dest, Path):
@@ -79,7 +97,6 @@ def link_one(verbose, force, src, dest):
 
     is_dir = src.is_dir()
     parent = Path(dest.parent)
-
     if not parent.exists():
         parent.mkdir(parents=True)
 
@@ -90,7 +107,6 @@ def link_one(verbose, force, src, dest):
 
     if force:
         print('Removed {}'.format(dest), file=out)
-
     try:
         dest.symlink_to(src, is_dir)
     except OSError:
