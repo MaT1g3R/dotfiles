d4c67b75adfa69ed7fd83ca2c476d7fa40f57dd4
MaT1g3R <982341777@qq.com>
2017-10-20 15:38:25 -0400

    Add colored output

--- C/mini_unit.c
+++ C/mini_unit.c
@@ -2,13 +2,18 @@
 
 int process_results(char *result, int tests_run)
 {
+	char *red = "\x1b[31m";
+	char *green = "\x1b[32m";
+	char *cyan = "\x1b[36m";
+	char *reset = "\x1b[0m";
+
 	if (result != 0)
-		printf("%s\n", result);
+		printf("%s%s%s\n", red, result, reset);
 
 	else
-		printf("ALL TESTS PASSED\n");
+		printf("%sALL TESTS PASSED%s\n", green, reset);
 
-	printf("Tests run: %d\n", tests_run);
+	printf("%sTests run: %d%s\n", cyan, tests_run, reset);
 
 	return result != 0;
 }
