acbea147c2ffe3a8dd4b7dd991807a1a957a7f27
Peijun Ma <peijun.ma@protonmail.com>
2018-01-03 17:23:18 -0500

    Update

--- scripts/update_pip.sh
+++ scripts/update_pip.sh
@@ -4,5 +4,6 @@ FROZEN=`pip freeze`
 FROZEN="${FROZEN//==/>=}"
 echo "$FROZEN" > tmp_pip.txt
 sed -i '/pygobject/d' ./tmp_pip.txt
+sed -i '/texttable/d' ./tmp_pip.txt
 pip install -Ur tmp_pip.txt
 rm tmp_pip.txt
