c390c84cf6f2959b52cef7ff7a055fbfc8cfe9cb
Peijun Ma <peijun.ma@protonmail.com>
2018-01-12 17:55:43 -0500

    Update update_pip.sh

--- scripts/update_pip.sh
+++ scripts/update_pip.sh
@@ -5,5 +5,6 @@ FROZEN="${FROZEN//==/>=}"
 echo "$FROZEN" > tmp_pip.txt
 sed -i '/pygobject/d' ./tmp_pip.txt
 sed -i '/texttable/d' ./tmp_pip.txt
+sed -i '/awscli/d' ./tmp_pip.txt
 pip install -Ur tmp_pip.txt
 rm tmp_pip.txt
