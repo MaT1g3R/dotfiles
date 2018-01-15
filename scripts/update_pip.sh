9679671d16ea62f8b9d03030dd821cd9436cf7e6
Peijun Ma <peijun.ma@protonmail.com>
2018-01-15 01:28:04 -0500

    Update update_pip.sh

--- scripts/update_pip.sh
+++ scripts/update_pip.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-FROZEN=`pip freeze`
+FROZEN=`pip3 freeze`
 FROZEN="${FROZEN//==/>=}"
 echo "$FROZEN" > tmp_pip.txt
 sed -i '/pygobject/d' ./tmp_pip.txt
 sed -i '/texttable/d' ./tmp_pip.txt
 sed -i '/awscli/d' ./tmp_pip.txt
-pip install -Ur tmp_pip.txt
+pip3 install -Ur tmp_pip.txt
 rm tmp_pip.txt
