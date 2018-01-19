57b8d1e232ec8419a27ccb50d7f10f44f2e66903
Peijun Ma <peijun.ma@protonmail.com>
2018-01-19 16:18:49 -0500

    Update "update_pip.sh"

--- scripts/update_pip.sh
+++ scripts/update_pip.sh
@@ -6,5 +6,6 @@ echo "$FROZEN" > tmp_pip.txt
 sed -i '/pygobject/d' ./tmp_pip.txt
 sed -i '/texttable/d' ./tmp_pip.txt
 sed -i '/awscli/d' ./tmp_pip.txt
+sed -i '/botocore/d' ./tmp_pip.txt
 pip3 install -Ur tmp_pip.txt
 rm tmp_pip.txt
