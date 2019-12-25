a3bf2d21d41449ef1cb41e3d4259cb49103690c6
Peijun Ma <peijun.ma@protonmail.com>
2019-12-25 18:06:43 -0500

    Track correct emacs bits

---
+++ emacs/emacs.d/init.el
@@ -0,0 +1,27 @@
+;-*-Emacs-Lisp-*-
+
+(package-initialize)
+
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package evil :ensure t
+  :config (require 'evil (evil-mode t))
+)
+
+(use-package magit :ensure t)
+
+(use-package helm :ensure t)
