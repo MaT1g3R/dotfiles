7a4a6dd7dc4197381404aa38894e9e3c1babcea6
Peijun Ma <peijun.ma@protonmail.com>
2019-12-25 20:00:07 -0500

    Load lisp directory

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,8 +1,24 @@
 ;-*-Emacs-Lisp-*-
 
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
+;; Leave this here, or package.el will just add it again.
 (package-initialize)
 
 (require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+;; Also add all directories within "lisp"
+;; I use this for packages I'm actively working on, mostly.
+(let ((files (directory-files-and-attributes "~/.emacs.d/lisp" t)))
+  (dolist (file files)
+    (let ((filename (car file))
+          (dir (nth 1 file)))
+      (when (and dir
+                 (not (string-suffix-p "." filename)))
+        (add-to-list 'load-path (car file))))))
 
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -24,4 +40,27 @@
 
 (use-package magit :ensure t)
 
+(use-package evil-magit :ensure t
+  :config (require 'evil-magit)
+)
+
 (use-package helm :ensure t)
+
+(use-package base16-theme
+  :ensure t
+  :config
+  (load-theme 'base16-oceanicnext t))
+
+;; Set the cursor color based on the evil state
+(defvar my/base16-colors base16-oceanicnext-colors)
+(setq evil-emacs-state-cursor   `(,(plist-get my/base16-colors :base0D) box)
+      evil-insert-state-cursor  `(,(plist-get my/base16-colors :base0D) bar)
+      evil-motion-state-cursor  `(,(plist-get my/base16-colors :base0E) box)
+      evil-normal-state-cursor  `(,(plist-get my/base16-colors :base0B) box)
+      evil-replace-state-cursor `(,(plist-get my/base16-colors :base08) bar)
+      evil-visual-state-cursor  `(,(plist-get my/base16-colors :base09) box))
+
+(use-package vterm :ensure t)
+
+(require 'init-fonts)
+(require 'liga-mode)
