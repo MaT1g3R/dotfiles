ab7d55216052a67dbb25cf477412d2b553e0922c
Peijun Ma <peijun.ma@protonmail.com>
2019-12-29 01:05:46 -0500

    Move most of config into config.org

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,18 +1,10 @@
 ;;; init.el -- My Emacs configuration
 ;-*-Emacs-Lisp-*-
 
-(require 'package)
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+;;; Commentary:
+;;; Short init.el to bootstrap config.org
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
-(setq package-enable-at-startup nil)
-(package-initialize)
-
-(defconst directories-to-load (list (expand-file-name "lisp" user-emacs-directory) "~/dotfiles_private/emacs.d"))
-
+;;; Code:
 ;; Add all directories within "lisp"
 (defun load-directory (directory)
   (add-to-list 'load-path directory)
@@ -24,107 +16,39 @@
 		   (not (string-suffix-p "." filename)))
 	  (add-to-list 'load-path (car file)))))))
 
+(defconst directories-to-load
+  (list
+    (expand-file-name "lisp" user-emacs-directory)
+    (expand-file-name "site-lisp" user-emacs-directory)
+    "~/dotfiles_private/emacs.d"))
+
 (dolist (dir directories-to-load) (load-directory dir))
 
+;; use different custom file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror)
 
+;; packages
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; workaround bug in Emacs 26.2
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+;; Setting up the package manager. Install if missing.
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
 (eval-when-compile
   (require 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t))
 
-;;; esc quits
-(defun minibuffer-keyboard-quit ()
-  "Abort recursive edit.
-In Delete Selection mode, if the mark is active, just deactivate it;
-then it takes a second \\[keyboard-quit] to abort the minibuffer."
-  (interactive)
-  (if (and delete-selection-mode transient-mark-mode mark-active)
-      (setq deactivate-mark  t)
-    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
-    (abort-recursive-edit)))
-
-
-(use-package evil
-  :ensure t
-  :init
-  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-  (setq evil-want-keybinding nil)
-  :config
-  (evil-mode 1))
-(global-set-key "\C-c\C-e" 'evil-mode)
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-(evil-define-key 'normal 'global (kbd "C-/") 'comment-line)
-(evil-define-key 'normal 'global "zs" 'my-save-word)
-
-(use-package evil-escape
-  :ensure t
-  :requires evil
-  :config
-  (evil-escape-mode t)
-  (setq-default evil-escape-key-sequence "ii"
-    evil-escape-delay 0.2))
-
-(use-package evil-collection
-  :after (evil helm)
-  :ensure t
-  :config
-  (setq evil-collection-setup-minibuffer t)
-  (evil-collection-init))
-
-(use-package magit :ensure t)
-
-(use-package evil-magit :ensure t
-  :config (require 'evil-magit))
-
-(use-package helm :ensure t)
-(require 'helm-config)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(evil-define-key 'normal 'global
-  ",b" 'helm-projectile-switch-to-buffer)
-(evil-define-key 'normal 'global
-  ";" 'helm-M-x)
-(evil-define-key 'normal 'global (kbd "C-f") 'helm-projectile-find-file)
-
-(use-package base16-theme
-  :ensure t
-  :config
-  (load-theme 'base16-oceanicnext t))
-
-(use-package all-the-icons :ensure t)
-
-;; Set the cursor color based on the evil state
-(defvar my/base16-colors base16-oceanicnext-colors)
-(setq evil-emacs-state-cursor   `(,(plist-get my/base16-colors :base0D) box)
-      evil-insert-state-cursor  `(,(plist-get my/base16-colors :base0D) bar)
-      evil-motion-state-cursor  `(,(plist-get my/base16-colors :base0E) box)
-      evil-normal-state-cursor  `(,(plist-get my/base16-colors :base0B) box)
-      evil-replace-state-cursor `(,(plist-get my/base16-colors :base08) bar)
-      evil-visual-state-cursor  `(,(plist-get my/base16-colors :base09) box))
-
-(use-package vterm :ensure t)
-(global-set-key "\M-t" 'projectile-run-vterm)
-
-(use-package emojify :ensure t)
-(add-hook 'after-init-hook #'global-emojify-mode)
-
-(use-package company :ensure t)
-(add-hook 'after-init-hook 'global-company-mode)
-
-(use-package org :ensure t)
-(require 'org)
-
-(use-package org-evil :ensure t)
-(require 'org-evil)
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 (require 'init-fonts)
@@ -136,3 +60,4 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (require 'init-spelling)
 
 (provide 'init)
+;;; init.el ends here
