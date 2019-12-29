8c6ef35e83172cb94687f9a68e9fb9b928e22ba5
Peijun Ma <peijun.ma@protonmail.com>
2019-12-28 23:08:37 -0500

    Set initial font size

--- emacs/emacs.d/lisp/init-fonts.el
+++ emacs/emacs.d/lisp/init-fonts.el
@@ -1,15 +1,15 @@
 ;;; Character sets
 
-(defcustom sanityinc/force-default-font-for-symbols nil
+(defcustom my/force-default-font-for-symbols nil
   "When non-nil, force Emacs to use your default font for symbols."
   :type 'boolean)
 
-(defun sanityinc/maybe-use-default-font-for-symbols ()
+(defun my/maybe-use-default-font-for-symbols ()
   "Force Emacs to render symbols using the default font, if so configured."
-  (when sanityinc/force-default-font-for-symbols
+  (when my/force-default-font-for-symbols
     (set-fontset-font "fontset-default" 'symbol (face-attribute 'default :family))))
 
-(add-hook 'after-init-hook 'sanityinc/maybe-use-default-font-for-symbols)
+(add-hook 'after-init-hook 'my/maybe-use-default-font-for-symbols)
 
 (add-to-list 'default-frame-alist '(font . "PragmataPro Mono Liga"))
 (set-face-attribute 'default t :font "PragmataPro Mono Liga")
@@ -17,38 +17,40 @@
 
 (require 'cl)
 
-(defun sanityinc/font-name-replace-size (font-name new-size)
+(defun my/font-name-replace-size (font-name new-size)
   (let ((parts (split-string font-name "-")))
     (setcar (nthcdr 7 parts) (format "%d" new-size))
     (mapconcat 'identity parts "-")))
 
-(defun sanityinc/set-frame-font-size (size)
-    (set-frame-font (sanityinc/font-name-replace-size (face-font 'default) size) t t))
+(defun my/set-frame-font-size (size)
+    (set-frame-font (my/font-name-replace-size (face-font 'default) size) t t))
 
-(defun sanityinc/increment-default-font-height (delta)
+(defun my/increment-default-font-height (delta)
   "Adjust the default font height by DELTA on every frame.
 Emacs will keep the pixel size of the frame approximately the
 same.  DELTA should be a multiple of 10, to match the units used
 by the :height face attribute."
   (let* ((new-height (+ (face-attribute 'default :height) delta))
          (new-point-height (/ new-height 10)))
-    (sanityinc/set-frame-font-size new-point-height)
+    (my/set-frame-font-size new-point-height)
     (set-face-attribute 'default nil :height new-height)
     (message "Default font size is now %d" new-point-height)))
 
-(defun sanityinc/increase-default-font-height ()
+(defun my/increase-default-font-height ()
   (interactive)
-  (sanityinc/increment-default-font-height 10)
+  (my/increment-default-font-height 10)
   (if (fboundp 'powerline-reset)
       (powerline-reset)))
 
-(defun sanityinc/decrease-default-font-height ()
+(defun my/decrease-default-font-height ()
   (interactive)
-  (sanityinc/increment-default-font-height -10)
+  (my/increment-default-font-height -10)
   (if (fboundp 'powerline-reset)
       (powerline-reset)))
 
-(bind-key (kbd "C-=") 'sanityinc/increase-default-font-height)
-(bind-key (kbd "C--") 'sanityinc/decrease-default-font-height)
+(my/set-frame-font-size 11)
+(set-face-attribute 'default nil :height 110)
+(bind-key (kbd "C-=") 'my/increase-default-font-height)
+(bind-key (kbd "C--") 'my/decrease-default-font-height)
 
 (provide 'init-fonts)
