ac3a09d82337f839176af4d2e83e9f4a64a76bb1
Peijun Ma <peijun.ma@protonmail.com>
2020-01-03 00:06:58 -0500

    Set t for mepla

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -51,7 +51,7 @@
 ;; packages
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (setq package-enable-at-startup nil)
 (package-initialize)
