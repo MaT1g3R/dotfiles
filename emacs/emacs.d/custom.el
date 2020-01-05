5fc696f56ac8af84cf7eb89a6c94d52ac8cf6367
Peijun Ma <peijun.ma@protonmail.com>
2020-01-04 20:50:58 -0500

    Install yaml-mode

--- emacs/emacs.d/custom.el
+++ emacs/emacs.d/custom.el
@@ -3,11 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flycheck-python-flake8-executable "python3")
+ '(flycheck-python-pycompile-executable "python3")
+ '(flycheck-python-pylint-executable "python3")
  '(ispell-personal-dictionary "~/dotfiles/emacs/emacs.d/word-list")
  '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (helm-lsp lsp-treemacs company-lsp lsp-ui lsp-mode helm-rg org-bullets kubernetes-evil kubernetes org-jira which-key diminish json-mode markdown-mode yasnippet-snippets highlight-escape-sequences highlight-operators highlight-numbers spaceline dashboard flycheck evil-commentary org-evil all-the-icons company treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs evil-escape emojify ## helm-projectile powerline-evil powerline key-chord evil-collection emojify-logos slack evil-magit base16-theme vterm helm magit evil use-package)))
+    (yaml-mode helm-lsp lsp-treemacs company-lsp lsp-ui lsp-mode helm-rg org-bullets kubernetes-evil kubernetes org-jira which-key diminish json-mode markdown-mode yasnippet-snippets highlight-escape-sequences highlight-operators highlight-numbers spaceline dashboard flycheck evil-commentary org-evil all-the-icons company treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs evil-escape emojify ## helm-projectile powerline-evil powerline key-chord evil-collection emojify-logos slack evil-magit base16-theme vterm helm magit evil use-package)))
  '(vterm-shell "/usr/bin/fish"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
