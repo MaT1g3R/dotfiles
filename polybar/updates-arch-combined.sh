0eb30887ca4d29acb50d05eece0895531e5c56c5
Peijun Ma <peijun.ma@protonmail.com>
2021-02-09 10:35:27 -0500

    diaaaaaazeeeeeeeeeepam

--- polybar/updates-arch-combined.sh
+++ polybar/updates-arch-combined.sh
@@ -5,7 +5,7 @@ if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
 fi
 
 # if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+if ! updates_aur=$(yay -Su --aur --quiet | wc -l); then
     updates_aur=0
 fi
 
