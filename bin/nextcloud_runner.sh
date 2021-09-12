0457b52463d606f39ba25955692b46bbc323b4e6
Peijun Ma <peijun.ma@protonmail.com>
2021-09-12 18:54:15 -0400

    hack

--- bin/nextcloud_runner.sh
+++ bin/nextcloud_runner.sh
@@ -3,18 +3,4 @@
 # See https://github.com/nextcloud/desktop/issues/1011
 #
 ############################################
-nohup kwalletmanager5 >> /dev/null 2>&1 &
-# Adjust the following to suit your needs
-WALLET="kdewallet"   # Name of wallet storing nextcloud client password
-MAX_TRIES=2          # Max. number of tries to ask for password
-############################################
-i=0
-while [ $i -lt $MAX_TRIES ]; do
-  ((i++))
-  open=$(qdbus org.kde.kwalletd5 /modules/kwalletd5 isOpen "$WALLET" | grep true)
-  if [ -n "$open" ]; then
-    break
-  fi
-  sleep 1
-done
-nohup nextcloud >> /dev/null 2>&1 &
+(sleep 10 && nextcloud) &
