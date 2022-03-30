8355158c74f4428a6ba8fb40393ba4b74a8b2d5e
Peijun Ma <peijun.ma@protonmail.com>
2022-03-30 13:03:34 -0400

    Add difft

--- shell/fish/conf.d/git.fish
+++ shell/fish/conf.d/git.fish
@@ -198,3 +198,7 @@ alias gpa='git pushall'
 alias gpp='git pull; git push'
 alias gssg="git status -s | awk -F / '{ print $NF, $0 }' | sed -e 's/\.scala/A/' | sort -k 1 | sed 's/.* //'"
 alias gsw='git switch'
+
+function git-difft
+    GIT_EXTERNAL_DIFF=difft git diff "$argv"
+end
